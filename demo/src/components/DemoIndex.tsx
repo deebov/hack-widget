e5bb85584739a095d5d13a7d1758aed1019b4a02
DILSHOD TUROBOV <deebov@yandex.com>
2022-10-14 19:38:06 +0500

    feat: remove nav

--- demo/src/components/DemoIndex.tsx
+++ demo/src/components/DemoIndex.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { navConfig } from '../navConfig';
-import { toPascalCase } from '../utils';
+import { Widget } from '../demo';
 
 export default function DemoIndex() {
   return (
     <div className="demo-index">
-      {navConfig.map((t) => (
-        <div className="demo-nav" key={t.title}>
-          <h2 className="demo-nav-title">{t.title}</h2>
-          <ul className="demo-nav-list">
-            {t.list.map((tt) => (
-              <li key={tt.code}>
-                <Link className="demo-nav-link" to={`/${tt.code}`}>
-                  {`${toPascalCase(tt.code)}`}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <Widget />
     </div>
   );
 }
